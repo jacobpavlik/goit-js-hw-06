3b36705ac8ad00a34b62dc00c0f4060906da24a3
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-04-07 20:15:47 +0200

    Added task-02, set EsLint to work

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach((ingredient, i) => {
+  const item = document.createElement('li');
+  item.innerText = ingredients[i];
+  item.classList.add('.item');
+  document.querySelector('ul').appendChild(item);
+});
